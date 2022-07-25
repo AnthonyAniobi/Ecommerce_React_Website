aee62f316d0c352d36466b1b4984fb74dc2db590
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-25 03:34:28 +0100

    worked on navigation in header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import React from 'react'
-import Header from './components/header/Header';
+import Header from './common/header/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
                         <button className="toggle" onClick={() => setMobileMenu(!MobileMenu)}>
                             {
                                 MobileMenu ? <i className="fas fa-times close home-bth"></i> :
-                                    <i className="fas fa-bars open"></i>
+                                    <i className="fa fa-bars open"></i>
                             }
                         </button>
                     </div>
